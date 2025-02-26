6ec4f5e2e1d9218bd3948034173f0ef0f8f54db1
kumar4532 <krantikumar2001@gmail.com>
2025-02-26 22:50:06 +0530

    handle cors error

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import referalRouter from './routes/refer.routes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5003
+
+app.use(cors({
+    origin: "https://accredian-frontend-task-three-ruby.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}))
 
 app.use(express.json())
 
